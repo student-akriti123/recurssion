fc34a77546910ccade7bce701380a3828baf74ca
student-akriti123 <akritisinha1602@gmail.com>
2023-12-10 20:16:21 +0530

    first commit

---
+++ stair path.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int stair(int n)
+{
+   if(n==1) return 1;
+   if(n==2)  return 2;
+
+   int totalways=stair(n-1)+stair(n-2);
+    return totalways ;
+}
+
+int main()
+{
+  int n;
+  printf("Enter the no. of ways\n");
+  scanf("%d",&n);
+
+    int ways=stair(n);
+    printf("%d",ways);
+    return 0;
+}
