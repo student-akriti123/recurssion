0e288284335f4b2cfcfe2eb94d7620d72a5b93a1
student-akriti123 <akritisinha1602@gmail.com>
2023-12-10 13:08:21 +0530

    first commit

---
+++ increasing parameter.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void increasing(int x,int n)
+{
+    if(x>n) return;
+    printf("%d\n",x);
+    increasing(x+1,n);
+    return ;
+}
+
+int main()
+{
+ int n;
+ printf("Enter the value of:");
+ scanf("%d",&n);
+ increasing(1,n);
+return 0;
+}
+
